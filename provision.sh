67c1b7a0d4405120017cf6cd708ef3a26a311a3e
Kevin Yeung <kyeung@thoughtworks.com>
2014-02-05 22:49:57 +0800

    Using NUS mirror to speed things up

--- provision.sh
+++ provision.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 echo "Provisioning"
 
+sed "s/us.archive.ubuntu.com/mirror.nus.edu.sg/" < /etc/apt/sources.list > s1.list
+sed "s/security.ubuntu.com/mirror.nus.edu.sg/" < s1.list > sources.list
+mv sources.list /etc/apt
+
 apt-get update
 apt-get install -y python-software-properties
 add-apt-repository ppa:git-core/ppa

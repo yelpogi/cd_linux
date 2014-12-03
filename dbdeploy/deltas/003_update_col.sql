95328715117577092f24f73c4889d80bc51c7c56
yelpogi <a_casala@yahoo.com>
2014-12-03 14:57:44 +0800

    Update 003_update_col.sql

--- dbdeploy/deltas/003_update_col.sql
+++ dbdeploy/deltas/003_update_col.sql
@@ -1,4 +1,4 @@
 update test
-SET NEWREMARK = "Ariel 1" where ID = 6;
+SET NEWREMARK = 'Ariel 1' where ID = 6;
 update test
-SET NEWREMARK = "Ariel 2" where ID = 7;
+SET NEWREMARK = 'Ariel 2' where ID = 7;
